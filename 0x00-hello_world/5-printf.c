94c30e8a23ac195cd537c6507823d66540162266
madoox1 <mouadidk@gmail.com>
2023-08-10 18:14:43 -0700

    modifing last 2 scripts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Enter point
+ * main - Entry point
  *
- * Return: Always 0 
+ * Return: Always 0
  */
-
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
-
