43e4d920ebba3eb3cc36469b047c76bee4b1034c
madoox1 <mouadidk@gmail.com>
2023-08-10 17:55:50 -0700

    correcting script 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main(void){
-        printf("with proper grammar, but the outcome is a piece of art,");
-        return (0);
+/**
+* main - Enter point
+*
+* Return: Always 0 
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
