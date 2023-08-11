7ece70d742998612349ffab0ae4f3a1bca91804f
madoox1 <mouadidk@gmail.com>
2023-08-11 13:36:06 -0700

    script 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 26; i++)
+	{
+		if( 'a' + i == 'q' || 'a' + i == 'e' )
+			continue ;
+		else
+			 putchar('a' + i );
+	}
+	printf("\n");
+	return (0);
+}
