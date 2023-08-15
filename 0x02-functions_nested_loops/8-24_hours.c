7c4647bfcd917fc3adde90349142a6bf062b77d6
madoox1 <mouadidk@gmail.com>
2023-08-15 13:55:33 -0700

    adding script 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
 #include "main.h"
-/* betty style doc for function main goes there */
+
 /**
- *jack_bauer - print every minute of the day
- *Return: no return value
- *@c: is the char to be checked
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i < 24; i++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (j = 0; j < 60; j++)
+		j = 0;
+		while (j < 60)
 		{
-			if (i < 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(':');	
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-				_putchar('\n');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
