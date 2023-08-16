0f73b9a1e5455eff399ab57053516807457e1a8b
madoox1 <mouadidk@gmail.com>
2023-08-16 12:03:08 -0700

    fixing script 11 and 9

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,77 +1,33 @@
 #include <stdio.h>
 #include "main.h"
-/* betty style doc for function main goes there */
-/**
- *_isalpha - check if character is a letter
- *Return: 1 if letter,0 otherwise
- *@c: is the char to be checked
- */
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
-	int i, k;
-    
-	if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i / 100 != 0)
-				_putchar(i / 100 + '0');
-			_putchar((i / 10) % 10 + '0');
-			_putchar(i % 10 + '0');
-			
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-		}
-	}
-	else
-	{
+	int i, j;
+
+	if (n <= 98)
+{
 		for (i = n; i <= 98; i++)
 		{
-			if (i < 10 && i >= 0)
-			{
-				_putchar(i + '0');
-				if (i != 98){
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else if (i >= 10 && i <= 98)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				if (i != 98){
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-			k = -i;
-			if (k < 10 && k > 0){
-				_putchar('-');
-				_putchar(k + '0');
-				if (k != 98){
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			else if (k >= 10)
-			{
-				_putchar('-');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-				if (k != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			}
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-_putchar('\n');
 }
+
