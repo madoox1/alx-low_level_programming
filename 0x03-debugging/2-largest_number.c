da0d96894a6dbebf6e5127701dc2f02b3e23a689
madoox1 <mouadidk@gmail.com>
2023-08-17 05:25:47 -0700

    fixing script 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,23 +7,15 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
+	if ( a >b )
+		largest=a;
 	else
-	{
-		largest = c;
-	}
-
-return (larges);
+		largest=b;
+	if (c > largest)
+		largest=c;
+return (largest);
 }
