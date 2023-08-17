f97d85f5dddc73690e31bde7a120206e5a44ece3
madoox1 <mouadidk@gmail.com>
2023-08-17 14:31:39 -0700

    adding script 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+if (size <= 0)
+	_putchar('\n');
+else
+{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j < (size - i - 1))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+}
