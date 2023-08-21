f75321eb8fd7a8dc7ad30126583a565a90064c89
madoox1 <mouadidk@gmail.com>
2023-08-21 10:00:55 -0700

    adding new scripts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string in inverse
+ * Return: none
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	for (i = 0 ; s[i] != '\0'; i++)
+		n++;
+	for (i = n; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * Return: none
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i, n;
+	char temp;
+
+	n = 0;
+	for (i = 0 ; s[i] != '\0'; i++)
+		n++;
+	for (i = 0; i < (n / 2); i++){
+		temp = s[i];
+		s[i] = s[n - i - 1];
+		s[n - i - 1] = temp;
+	}
+}
