5e5137bf359bd1d0a12f660c203b19a110fb7af8
madoox1 <mouadidk@gmail.com>
2023-09-05 12:47:28 -0700

    fixing errors in last scripts

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,23 +8,17 @@
  */
 char *_strdup(char *str)
 {
-	int size = 0 ,i  ;
+	int i, size = 0;
 	char *str2 = NULL;
 
 	if (str == NULL)
 		return (NULL);
-	else
-	{
-		for (i = 0; str[i]; i++)
-			size++;
-		str2 = malloc(size+1);
-		if (str2 == NULL)
-			return (NULL);
-		else
-		{
-			for(i = 0; i < size; i++)
-				str2[i] = str[i];
-			return (str2);
-		}
-	}
+	while (str[size] != '\0')
+		size++;
+	str2 = malloc(size + 1);
+	if (str2 == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		str2[i] = str[i];
+	return (str2);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,22 +8,27 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i , size1 = 0, size2 = 0;
+	int i = 0, size = 0;
 	char *str = NULL;
 
-	for (i =0; s1[i]; i++)
-		size1++;
-	for (i =0; s2[i]; i++)
-		size2++;
-	str = malloc(size1 + size2 + 1);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i]; i++)
+		size++;
+	for (i = 0; s2[i]; i++)
+		size++;
+	str = malloc(size + 1);
 	if (str == NULL)
 		return (NULL);
-	else
+	size = 0;
+	for (i = 0; s1[i]; i++)
 	{
-		for (i = 0; i < size1; i++)
-			str[i] = s1 [i];
-		for (i = 0; i < size2; i++)
-			str[i + size1] = s2[i];
-	return (str);
+		size++;
+		str[i] = s1[i];
 	}
+	for (i = 0; s2[i]; i++)
+		str[i + size] = s2[i];
+	return (str);
 }
