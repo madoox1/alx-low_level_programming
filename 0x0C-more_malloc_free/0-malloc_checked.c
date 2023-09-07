5dc4645cb5adea9c5ca5d4197b8935593da97687
madoox1 <mouadidk@gmail.com>
2023-09-07 13:35:19 -0700

    script 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory
+ * @b: size
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
