9c65c775e7c2307a276799e33bd48906432a7a2a
madoox1 <mouadofox88@gmail.com>
2024-01-21 03:23:04 -0800

    test

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,24 @@
-include<stdio.h>
+#include <stdio.h>
 /**
- * checker - check from base to input number
- * @n : number to start from
- * @base : number to determinate square
- * Return : natureal square root
+ * checker - checks the input number from n to the base
+ * @n: number is squared and compared against base
+ * @base: base number to check
+ * Return: natural square root of number base
  */
 int checker(int n, int base)
 {
-	if(n * n == base)
-		return(n);
-	else if(n * n > base)
-		return(-1);
-	return checker(n + 1, base);
+	if (n * n == base)
+		return (n);
+	if (n * n > base)
+		return (-1);
+	return (checker(n + 1, base));
 }
 /**
- * _sqrt_recursion - returns natural square root
- * @n : number to determinate natural square root
- * Return : the square root of n
+ * _sqrt_recursion - return the natural square root of a number n.
+ * @n: number to check for square roots.
+ * Return: the natural square root of number n
  */
 int _sqrt_recursion(int n)
 {
-	return checker(1, n);
+	return (checker(1, n));
 }
-
